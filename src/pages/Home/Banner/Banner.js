c165fd1b5e20319b33b695b2540001bcc0ec170e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-26 22:09:27 +0600

    header added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import './Banner.css'
+
+// banner 
+import banner1 from '../../../images/banner/banner1.jpg'
+import banner2 from '../../../images/banner/banner2.jpg'
+import banner3 from '../../../images/banner/banner3.jpg'
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
+
     return (
-        <div>
-            <h2>This is banner section</h2>
+        <div className=''>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption >
+                        <h3>Lamborghini Diablo</h3>
+                        <p>Independent automobile repair shops  in the US may also achieve certification through manufacturer sponsored programs</p>
+                        <button className='btn btn-danger'>Explore More</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Ford Raptor</h3>
+                        <p>Permits motorists more flexibility in selecting where their car is serviced.</p>
+                        <button className='btn btn-danger'>Explore More</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Porsche 911 Carrera</h3>
+                        <p>A choice in service centers to select from; service department at a car dealership</p>
+                        <button className='btn btn-danger'>Explore More</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../../../components/CustomLink/CustomLink';
+import logo from '../../../images/goodwish-logo-light.png'
 import './Header.css'
+
 const Header = () => {
+
     return (
         <div>
-            <h2>This is header</h2>
+            <Navbar collapseOnSelect expand="lg"  className='header-container'  variant="dark">
+                <Container >
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo} height="30" alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav className='nav-style my-1'>
+                            <Nav.Link as={CustomLink} to='/donate' className='mt-2 fw-bold'>Donation</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/events' className='mt-2 fw-bold'>Events</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/blog' className='mt-2 fw-bold'>Blog</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold'>Login</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/signup' className='mt-2 fw-bold'>Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
