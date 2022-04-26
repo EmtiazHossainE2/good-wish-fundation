904084ec644fead2a416fbf0aeac07bdd3cba9e1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-26 18:30:07 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import Blog from './pages/Blog/Blog';
+import Events from './pages/Events/Events';
+import Donate from './pages/Donate/Donate';
+import Login from './pages/Accounts/Login/Login';
+import SignUp from './pages/Accounts/SignUp/SignUp';
+import Header from './pages/Shared/Header/Header';
+// import Footer from './pages/Shared/Footer/Footer';
+import NotFound from './pages/Shared/NotFound/NotFound';
 
 const App = () => {
     return (
         <div>
-            <h2 className='text-center'>GoodWish</h2>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}> Home </Route>
+                <Route path='/blog' element={<Blog/>}>  </Route>
+                <Route path='/events' element={<Events/>}>  </Route>
+                <Route path='/donate' element={<Donate/>}>  </Route>
+                <Route path='/login' element={<Login/>}>  </Route>
+                <Route path='/signup' element={<SignUp/>}>  </Route>
+                <Route path='*' element={<NotFound/>}></Route>
+            </Routes>
+            {/* <Footer/> */}
         </div>
     );
 };
---
+++ src/pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <div className="text-center">
+                <h2>This page is not available 404 </h2>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
