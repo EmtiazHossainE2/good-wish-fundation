fa27bc06044c6fcd36801605a0a2505ed0c8caa6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-27 14:10:04 +0600

    new cause form validation added

--- src/pages/AddCauses/AddCauses.js
+++ src/pages/AddCauses/AddCauses.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
+import './AddCauses.css'
+import { FaHandHoldingHeart } from 'react-icons/fa';
+
+
 
 const AddCauses = () => {
     const [validated, setValidated] = useState(false);
+    const [terms, setTerms] = useState(false)
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
@@ -14,14 +19,14 @@ const AddCauses = () => {
         setValidated(true);
     };
     return (
-        <div className='mt-5 py-5 container'>
+        <div className='mt-5 py-5  add-container'>
             <h2 className='text-center pt-3 '>Add New Cause</h2>
             <div className='hr-style mx-auto pt-1 '>
                 <hr />
             </div>
-            <div className="container w-75 mx-auto py-5">
+            <div className="container w-75 mx-auto py-5 add-form">
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Row className="mb-3">
+                    <Row className="mb-3 fst-italic fw-bold">
                         <Form.Group as={Col} md="6" controlId="validationCustomUsername">
                             <Form.Label>Cause Title</Form.Label>
                             <InputGroup hasValidation>
@@ -51,10 +56,10 @@ const AddCauses = () => {
                             </InputGroup>
                         </Form.Group>
                     </Row>
-                    <Row className="mb-3">
+                    <Row className="mb-3 mt-4 fst-italic">
                         <Form.Group as={Col} md="6" controlId="validationCustom03">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" placeholder="Enter description" required />
+                            <Form.Control as="textarea" rows={3} type="text" placeholder="Enter description" required />
                             <Form.Control.Feedback type="invalid" className='fw-bold'>
                                 Please Provide Description
                             </Form.Control.Feedback>
@@ -67,15 +72,15 @@ const AddCauses = () => {
                             </Form.Control.Feedback>
                         </Form.Group>
                     </Row>
-                    <Form.Group className="mb-3">
-                        <Form.Check
-                            required
-                            label="Agree to terms and conditions"
-                            feedback="You must agree before submitting."
-                            feedbackType="invalid"
-                        />
-                    </Form.Group>
-                    <Button type="submit">Submit form</Button>
+                    <div className='d-flex '>
+                        <div className='fw-bold text-primary mb-3'>
+                            <input onClick={() => setTerms(!terms)} type="checkbox" name="terms" id="terms" />
+                            <label className={terms ? 'ps-1 text-primary' : 'ps-1 text-danger'} htmlFor="terms" style={{ cursor: 'pointer' }}>Accept Terms and Conditions</label>
+                        </div>
+                    </div>
+                    <Button disabled={!terms} variant="warning" type="submit" className=' fs-5'>
+                        Submit <span className='ps-3 '><FaHandHoldingHeart/></span> 
+                    </Button>
                 </Form>
             </div>
         </div>
