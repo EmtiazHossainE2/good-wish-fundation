d40361b767ad44233a918eeed2fad218ebe5e281
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-28 02:04:36 +0600

    donation form error handle

--- src/pages/Donation/Donation.js
+++ src/pages/Donation/Donation.js
@@ -1,28 +1,37 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import auth from '../../Firebase/firebase.init';
 import useSingleDetail from '../../hooks/useSingleDetail';
+import { FaHandHoldingHeart } from 'react-icons/fa';
 import './Donation.css'
 
 const Donation = () => {
     const { detailsId } = useParams()
-    const [singleDetail, setSingleDetail] = useSingleDetail(detailsId)
+    const [singleDetail] = useSingleDetail(detailsId)
     const [user, loading, error] = useAuthState(auth);
+    const [terms, setTerms] = useState(false)
 
-    const handlePlaceOrder = event => {
+    const handleDonate = event => {
         event.preventDefault()
-        const order = {
+        const donate = {
             name: user.displayName,
             email: user.email,
-            // service: service.name,
-            // serviceId: serviceId,
+            charity: singleDetail.title,
+            detailsId: detailsId,
+            money: event.target.money.value,
+            date: event.target.date.value,
             address: event.target.address.value,
             phone: event.target.number.value
         }
-        axios.post('https://stark-beach-29284.herokuapp.com/order', order)
+        if (donate.email === '' || donate.charity === '' || donate.money === '' || donate.date === '' || donate.address === '' ) {
+            toast.error(`Please add all info `, { id: "info" });
+        }
+        else {
+            axios.post('', donate)
             .then(response => {
                 const { data } = response
                 if (data.insertedId) {
@@ -34,6 +43,7 @@ const Donation = () => {
             .catch((error) => {
                 console.log(error);
             });
+        }
 
     }
 
@@ -45,15 +55,30 @@ const Donation = () => {
                 </div>
             </div>
             <div className='my-5 text-center m-auto donation-form'>
-                <form onSubmit={handlePlaceOrder}>
+                <Form noValidate  onSubmit={handleDonate}>
                     <input className='w-75 mb-3 py-2' type="text" value={user?.displayName} name='name' placeholder='Name' required disabled /> <br />
                     <input className='w-75 mb-3 py-2' type="email" value={user?.email} name='email' placeholder='Email' required disabled /> <br />
-                    <input className='w-75 mb-3 py-2' type="text" value={singleDetail?.title} name='service' placeholder='service' required /> <br />
-                    <input className='w-75 mb-3 py-2' type="number" name='money' placeholder='Enter Money ' required /> <br />
+                    <input className='w-75 mb-3 py-2' type="text" value={singleDetail?.title} name='charity' placeholder='Charity' required /> <br />
+                    <div className="row w-100 text-center mx-auto ">
+                        <div className="col-md-6 ps-5">
+                            <input className=' mb-3 py-2' type="number" name='money' placeholder='Enter Money ' required />
+                        </div>
+                        <div className="col-md-6 ps-4">
+                            <input className=' mb-3 py-2' type="date" name='date' placeholder='Date ' required />
+                        </div>
+                    </div>
                     <input className='w-75 mb-3 py-2' type="text" name='address' placeholder='Address' required /> <br />
                     <input className='w-75 mb-3 py-2' type="number" name='number' placeholder='Contact Number' required /> <br />
-                    <input className='w-75 btn btn-primary' type="submit" value="Donate Now"  />
-                </form>
+                    <div className='d-flex w-75 mx-auto'>
+                        <div className='fw-bold text-primary mb-3'>
+                            <input onClick={() => setTerms(!terms)} type="checkbox" name="terms" id="terms" />
+                            <label className={terms ? 'ps-1 text-primary' : 'ps-1 text-danger'} htmlFor="terms" style={{ cursor: 'pointer' }}>Accept Terms and Conditions</label>
+                        </div>
+                    </div>
+                    <Button disabled={!terms} variant="warning" type="submit" className='w-75 fs-5'>
+                        Donate <span className='ps-3 '><FaHandHoldingHeart /></span>
+                    </Button>
+                </Form>
             </div>
         </div>
     );
