a45d53c72cdef29a34d8778ce9ed69d0a19fc9fa
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-27 20:34:22 +0600

    error handle and user login out navbar handle

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,15 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import CustomLink from '../../../components/CustomLink/CustomLink';
+import auth from '../../../Firebase/firebase.init';
 import logo from '../../../images/goodwish-logo-light.png'
 import './Header.css'
 
 const Header = () => {
-
+    const [user] = useAuthState(auth)
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg"  fixed='top' className='header-container'  variant="dark">
+            <Navbar collapseOnSelect expand="lg" fixed='top' className='header-container' variant="dark">
                 <Container >
                     <Navbar.Brand as={Link} to="/">
                         <img src={logo} height="30" alt="" />
@@ -19,12 +22,52 @@ const Header = () => {
                         <Nav className="me-auto"></Nav>
                         <Nav className='nav-style my-1'>
                             <Nav.Link as={CustomLink} to='/donate' className='mt-2 fw-bold'>Donation</Nav.Link>
-                            <Nav.Link as={CustomLink} to='/add-causes' className='mt-2 fw-bold'>Add Causes</Nav.Link>
-                            <Nav.Link as={CustomLink} to='/manage-cause' className='mt-2 fw-bold'>Manage Causes</Nav.Link>
+                            {user?.email
+                                && <>
+                                    <Nav.Link as={CustomLink} to='/add-causes' className='mt-2 fw-bold'>Add Causes</Nav.Link>
+                                    <Nav.Link as={CustomLink} to='/manage-cause' className='mt-2 fw-bold'>Manage Causes</Nav.Link>
+
+                                </>
+                            }
+                            {/* {user?.uid
+                                ?
+                                ""
+                                :
+                                <Nav.Link as={CustomLink} to='/about'>About</Nav.Link>
+                            } */}
+
                             <Nav.Link as={CustomLink} to='/events' className='mt-2 fw-bold'>Events</Nav.Link>
                             <Nav.Link as={CustomLink} to='/blog' className='mt-2 fw-bold'>Blog</Nav.Link>
-                            <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold'>Login</Nav.Link>
-                            <Nav.Link as={CustomLink} to='/signup' className='mt-2 fw-bold'>Register</Nav.Link>
+
+                            {user?.uid
+                                ?
+                                <NavDropdown className='pt-2 fs-bold  ' title="Profile" id="collasible-nav-dropdown">
+                                    <Nav.Link as={CustomLink} to='/profile' className='mt-2 fw-bold bg-dark text-light'>Dashboard</Nav.Link>
+                                    <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold bg-dark text-light' onClick={() => signOut(auth)}>Log Out</Nav.Link>
+                                </NavDropdown>
+                                :
+                                <>
+                                    <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold'>Login</Nav.Link>
+                                    <Nav.Link as={CustomLink} to='/signup' className='mt-2 fw-bold'>Register</Nav.Link>
+                                </>
+                            }
+
+                            {/* <NavDropdown className='p-0 text-light' title="Account" id="collasible-nav-dropdown">
+                                {user?.email
+                                    ?
+                                    <Nav.Link as={CustomLink} to='/profile' className='mt-2 fw-bold'>Your Profile</Nav.Link>
+                                    :
+                                    <Nav.Link as={CustomLink} to='/signup' className='mt-2 fw-bold'>Register</Nav.Link>
+                                }
+                                {user?.email
+                                    ?
+                                    <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold' onClick={() => signOut(auth)}>Log Out</Nav.Link>
+                                    :
+                                    <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold'>Login</Nav.Link>
+                                }
+                            </NavDropdown> */}
+
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
