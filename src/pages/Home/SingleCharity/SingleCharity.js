d5bd16a16161b0225eaca72e6d8529f9448d4636
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-27 18:14:24 +0600

    every single item bg color set

--- src/pages/Home/SingleCharity/SingleCharity.js
+++ src/pages/Home/SingleCharity/SingleCharity.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SingleCharity = ({ charity }) => {
-    const { title, img , _id } = charity
+    const { title, img , _id , color } = charity
     return (
         <div className='col-md-3' >
             <div className="cart text-center rounded-1  bg-warning">
@@ -10,7 +10,7 @@ const SingleCharity = ({ charity }) => {
                     <div className=''>
                         <img className='w-100' src={img} alt="" />
                     </div>
-                    <button className='w-100 py-3  btn btn-warning'><span className='fw-bold fst-italic'>{title}</span></button>
+                    <button style={{backgroundColor : color , border: '0'}} className={`w-100 py-3 text-light ${color ? 'color' : 'btn btn-warning'}`}><span className='fw-bold fst-italic'>{title}</span></button>
                 </Link>
             </div>
         </div>
